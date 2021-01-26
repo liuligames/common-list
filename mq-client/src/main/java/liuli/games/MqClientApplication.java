50af2af94d91043a104629add313de9c08228d8c
LiuShaowen <liuligames0916@gmail.com>
2021-01-26 10:37:56 +0800

    add mq demo

---
+++ mq-client/src/main/java/liuli/games/MqClientApplication.java
@@ -0,0 +1,11 @@
+package liuli.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqClientApplication.class, args);
+    }
+}
